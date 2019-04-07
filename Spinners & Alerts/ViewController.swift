23b2344c3fb78e5233829f14cf616cac67303d65
Tom <melooh42@gmail.com>
2019-04-07 22:23:04 +0900

    Initial Commit

---
+++ Spinners & Alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spinners & Alerts
+//
+//  Created by 板垣智也 on 2019/04/07.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
